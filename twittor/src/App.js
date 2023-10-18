8cd4b0f2f6afb7f6435b1def7f6cb8ee8c705e6b
Matu36 <matipineda85@live.com.ar>
2023-10-18 12:39:15 -0300

    sass

--- twittor/src/App.js
+++ twittor/src/App.js
@@ -1,9 +1,24 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
-      <h1>HEY DUDE!</h1>
+      <div className="App-header">
+        <h1>HEY DUDE!</h1>
+        <h3> Clickeaste {count} veces </h3>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Apretá ACÁ!
+        </button>
+      </div>
     </div>
   );
 }
