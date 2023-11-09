1ac9a8bd3462df3bf397cab47e705bf976ce3ab1
Matu36 <matipineda85@live.com.ar>
2023-11-09 11:55:39 -0300

    varios

--- twittor/src/routes/Routing.js
+++ twittor/src/routes/Routing.js
@@ -4,17 +4,16 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { map } from "lodash";
 import configRouting from "./configRouting";
 
-export default function Routing() {
+export default function Routing(props) {
+  const { setRefreshCheckLogin } = props;
+
   return (
     <Router>
       <Switch>
         {map(configRouting, (route, index) => (
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            component={route.component}
-          ></Route>
+          <Route key={index} path={route.path} exact={route.exact}>
+            <route.component setRefreshCheckLogin={setRefreshCheckLogin} />
+          </Route>
         ))}
       </Switch>
     </Router>
--- twittor/src/routes/configRouting.js
+++ twittor/src/routes/configRouting.js
@@ -1,7 +1,14 @@
 import Home from "../page/Home/Home";
 import Error404 from "../page/Error404/Error404";
+import User from "../page/User";
 
 export default [
+  {
+    path: "/:id",
+    exact: true,
+    component: User,
+  },
+
   {
     path: "/",
     exact: true,
