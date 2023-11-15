597201c9d7d873ef70be2f78017a039f00acdd2b
Matu36 <matipineda85@live.com.ar>
2023-11-15 13:17:53 -0300

    error

--- twittor/src/page/Error404/Error404.js
+++ twittor/src/page/Error404/Error404.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Error404Image from "../../assets/png/error-404.png";
+import Logo from "../../assets/png/logo.png";
 
 import "./Error404.scss";
 
 export default function Error404() {
-  return <div>Error404</div>;
+  return (
+    <div className="error404">
+      <img src={Logo} alt="twittor" />
+      <img src={Error404Image} alt="Error404" />
+      <Link to="/">Volver al Inicio </Link>
+    </div>
+  );
 }
