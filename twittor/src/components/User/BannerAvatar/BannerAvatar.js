10fbc744860c76f5d31bff2f41b4c11f6b073bc6
Matu36 <matipineda85@live.com.ar>
2023-11-16 11:40:09 -0300

    followers

--- twittor/src/components/User/BannerAvatar/BannerAvatar.js
+++ twittor/src/components/User/BannerAvatar/BannerAvatar.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import ConfigModal from "../../modal/BasicModal/ConfigModal/ConfigModal";
 import EditUserForm from "../EditUserForm/EditUserForm";
 import { API_HOST } from "../../../utils/constant";
 import AvatarNoFound from "../../../assets/png/avatar-no-found.png";
+import { checkFollowApi } from "../../../api/follow";
 
 import "./BannerAvatar.scss";
 
 export default function BannerAvatar(props) {
   const { user, loggedUser } = props;
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false); //Estado para mostrar o no el modal
+  const [following, setFollowing] = useState(null); //Estado que guarda si estamos siguiendo a alguien o no
 
   const bannerUrl = user?.banner
     ? `${API_HOST}/obtenerBanner?id=${user.id}`
@@ -18,6 +20,16 @@ export default function BannerAvatar(props) {
   const avatarUrl = user?.avatar
     ? `${API_HOST}/obtenerAvatar?id=${user.id}`
     : AvatarNoFound;
+
+  useEffect(() => {
+    checkFollowApi(user?.id).then((response) => {
+      if (response?.status) {
+        setFollowing(true);
+      } else {
+        setFollowing(false);
+      }
+    });
+  }, [user]);
 
   return (
     <div
@@ -34,8 +46,12 @@ export default function BannerAvatar(props) {
           {/* {loggedUser._id === user.id &&  */}
           <Button onClick={() => setShowModal(true)}>Editar Perfil</Button>
 
-          {/* {loggedUser._id !== user.id && */}
-          <Button>Seguir</Button>
+          {/* {loggedUser._id !== user.id && (
+            following !== null &&
+            (following ? ( */}
+          <Button>Siguiendo</Button>
+          {/* ) : ( */}
+          <Button>Seguir </Button>
         </div>
       )}
 
