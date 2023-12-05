d09be39c040526b02cbeeeb69870b93e5653e44f
Matu36 <matipineda85@live.com.ar>
2023-12-05 10:49:12 -0300

    MEDIA

--- twittor/src/components/ListUsers/User.js
+++ twittor/src/components/ListUsers/User.js
@@ -5,6 +5,63 @@ import { API_HOST } from "../../utils/constant";
 import { getUserApi } from "../../api/user";
 import AvatarNoFound from "../../assets/png/avatar-no-found.png";
 
+//REEMPLAZAR EL MEDIA CON EL FLEX
+
+/*
+
+EJEMPLO:
+<body>
+  <div class="container">
+    <div class="row comments justify-content-center">
+      <div class="col-8">
+        <form class="form_comments d-flex justify-content-end flex-wrap">
+          <textarea placeholder="Comentario"></textarea>
+          <button class="btn" type="button">Comentar</button>
+        </form>
+        <!--conservé "media" ya que aparece en los estilos; flex-direction-column para tomar la dirección de arriba para abajo (no de izquierda a derecha)-->
+        <div class="media d-flex flex-direction-column">
+          <div class="flex-shrink-0">
+            <img src="https://via.placeholder.com/64" alt="Karina" class="align-self-center">
+          </div>
+          <!--flex-grow se utiliza para que un elemento flex crezca hasta rellenar el espacio disponible
+          ms (margin start) reemplaza a ml (margin left): https://getbootstrap.com/docs/5.0/getting-started/rtl/#approach-->
+          <div class="flex-grow-1 ms-0">
+            <p class="name">Karina ST <span>11:56, hoy</span></p>
+            <p class="comment">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis eos dolore optio!
+
+            </p>
+            <!--flex-direction-row para tomar la dirección de izquierda a derecha; justify-content-end para alinear los componentes a la derecha-->
+            <div class="buttons text-end d-flex flex-direction-row justify-content-end">
+              <a href="#">Responder</a>
+              <a href="#">Editar</a>
+              <a href="#">Eliminar</a>
+            </div>
+
+            <div class="media d-flex flex-direction-column">
+              <!--flex-shrink por su parte se utiliza para que un elemento flex se encoja, de ser necesario. Si su valor es cero, no se encogerá cuando la página se haga más pequeña -->
+              <div class="flex-shrink-0">
+                <img src="https://via.placeholder.com/64" alt="Karina" class="d-flex align-self-center">
+              </div>
+              <div class="flex-grow-1 ms-0">
+                <p class="name">Karina <span>22:56, hoy</span></p>
+                <p class="comment">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis eos dolore optio!
+                </p>
+                <div class="buttons text-end d-flex flex-direction-row justify-content-end">
+                  <a href="#">Responder</a>
+                  <a href="#">Editar</a>
+                  <a href="#">Eliminar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>*/
+
 export default function User(props) {
   const { user } = props;
   const [userInfo, setUserInfo] = useState(null);
